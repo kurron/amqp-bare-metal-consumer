264cdfc7ddff7a7779074c7802610e1a616fee60
Ron Kurr <kurr@kurron.org>
2017-05-11 15:29:33 -0400

    Printout duration as we process.

--- src/main/groovy/org/kurron/bare/metal/consumer/Consumer.groovy
+++ src/main/groovy/org/kurron/bare/metal/consumer/Consumer.groovy
@@ -4,13 +4,29 @@ import groovy.util.logging.Slf4j
 import org.springframework.amqp.core.Message
 import org.springframework.amqp.core.MessageListener
 
+import java.time.Duration
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.atomic.AtomicLong
+
 /**
  * Processes messages.
  */
 @Slf4j
 class Consumer implements MessageListener {
+
+    def counter = new AtomicInteger( 0 )
+    def start = new AtomicLong( 0 )
+    def now = new AtomicLong( 0 )
+    def duration = new AtomicLong( 0 )
+
     @Override
     void onMessage( Message message ) {
-        log.debug( message.messageProperties.correlationIdString )
+        // only store the first timestamp
+        start.compareAndSet( 0, System.currentTimeMillis() )
+        now.set( System.currentTimeMillis() )
+        def currentValue = counter.incrementAndGet()
+        def durationMillis = duration.addAndGet( now.get() - start.get() )
+        def durationISO = Duration.ofMillis( durationMillis )
+        0 == currentValue % 2000 ? log.info( '{} messages has taken {} milliseconds to process', currentValue, durationMillis ) : ''
     }
 }
